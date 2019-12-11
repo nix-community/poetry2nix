a051e38eef9ae9fe73183e98150eb3b43350ce75
zimbatm <zimbatm@zimbatm.com>
2019-12-11 13:12:38 +0100

    add shell.nix
    I wanted to have the latest poetry to work on the tests

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import (fetchTarball { url = "channel:nixpkgs-unstable"; }) {};
+  poetry2nix = import ./. { inherit pkgs; inherit poetry; };
+  poetry = pkgs.callPackage ./pkgs/poetry { python = pkgs.python3; inherit poetry2nix; };
+in
+  pkgs.mkShell {
+    buildInputs = [
+      poetry
+    ];
+  }
