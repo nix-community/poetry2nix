3e08afdd212ed94d445b393afb60869a7122fd62
zimbatm <zimbatm@zimbatm.com>
2019-12-11 13:20:15 +0100

    test: add git-deps

--- tests/default.nix
+++ tests/default.nix
@@ -8,4 +8,5 @@ in
   override = pkgs.callPackage ./override-support { inherit poetry2nix; };
   top-packages-1 = pkgs.callPackage ./common-pkgs-1 { inherit poetry2nix; };
   top-packages-2 = pkgs.callPackage ./common-pkgs-2 { inherit poetry2nix; };
+  git-deps = pkgs.callPackage ./git-deps { inherit poetry2nix; };
 }
