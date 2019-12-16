e529d261096402b222d06081dee5f72e300bb743
Tobias Pflug <tobias.pflug@gmail.com>
2019-12-16 19:01:16 +0100

    Disable manylinux test
    The manylinux support works but needs to remain disabled until the
    necessary nixpkgs changes have been merged and propagated.

--- tests/default.nix
+++ tests/default.nix
@@ -14,5 +14,8 @@ in
   top-packages-1 = pkgs.callPackage ./common-pkgs-1 { inherit poetry2nix; };
   top-packages-2 = pkgs.callPackage ./common-pkgs-2 { inherit poetry2nix; };
   pep425 = pkgs.callPackage ./pep425 { inherit pep425; inherit pep425OSX; inherit pep425Python37; };
-  manylinux = pkgs.callPackage ./manylinux { inherit poetry2nix; };
+
+  # manylinux requires nixpkgs with https://github.com/NixOS/nixpkgs/pull/75763
+  # Once this is available in 19.09 and unstable we can re-enable the manylinux test
+  #manylinux = pkgs.callPackage ./manylinux { inherit poetry2nix; };
 }
