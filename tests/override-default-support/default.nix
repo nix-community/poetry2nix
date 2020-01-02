f73807d3dc8d7b414d9a663d366971f53ebf540c
adisbladis <adisbladis@gmail.com>
2020-01-02 22:02:08 +0000

    Add missing test files

---
+++ tests/override-default-support/default.nix
@@ -0,0 +1,24 @@
+{ lib, python3, poetry2nix, runCommand }:
+
+let
+  p = poetry2nix.mkPoetryApplication {
+    python = python3;
+    src = ./.;
+    poetrylock = ./poetry.lock;
+    pyproject = ./pyproject.toml;
+    overrides = [
+      (poetry2nix.defaultPoetryOverrides.overrideOverlay (self: super: {
+        alembic = super.alembic.overrideAttrs (
+          old: {
+            TESTING_FOOBAR = 42;
+          }
+        );
+      }))
+    ];
+  };
+in
+runCommand "test" {} ''
+  x=${builtins.toString (p.python.pkgs.alembic.TESTING_FOOBAR)}
+  [ "$x" = "42" ] || exit 1
+  mkdir $out
+''
