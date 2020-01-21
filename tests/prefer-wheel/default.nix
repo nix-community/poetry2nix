3ade93a0e13ef817ca47b0f864afb068cad52f90
adisbladis <adisbladis@gmail.com>
2020-01-21 12:30:41 +0000

    Assert URL actually wheel url in preferWheel test

--- tests/prefer-wheel/default.nix
+++ tests/prefer-wheel/default.nix
@@ -1,20 +1,26 @@
 { lib, poetry2nix, python3 }:
 
-poetry2nix.mkPoetryApplication {
-  python = python3;
-  pyproject = ./pyproject.toml;
-  poetrylock = ./poetry.lock;
-  src = lib.cleanSource ./.;
-  overrides = [
-    poetry2nix.defaultPoetryOverrides
-    # This is also in overrides.nix but repeated for completeness
-    (
-      self: super: {
-        maturin = super.maturin.override {
-          preferWheel = true;
-        };
-      }
-    )
+let
+  drv = poetry2nix.mkPoetryApplication {
+    python = python3;
+    pyproject = ./pyproject.toml;
+    poetrylock = ./poetry.lock;
+    src = lib.cleanSource ./.;
+    overrides = [
+      poetry2nix.defaultPoetryOverrides
+      # This is also in overrides.nix but repeated for completeness
+      (
+        self: super: {
+          maturin = super.maturin.override {
+            preferWheel = true;
+          };
+        }
+      )
 
-  ];
-}
+    ];
+  };
+
+  url = lib.elemAt drv.passthru.python.pkgs.maturin.src.urls 0;
+
+in
+  assert lib.hasSuffix "whl" url; drv
