6fc16e0deedad5d7ebb53531ab14d67ce22ea56e
adisbladis <adisbladis@gmail.com>
2020-02-27 08:13:56 +0000

    Use overlay in shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
+{ pkgs ? import (fetchTarball { url = "channel:nixpkgs-unstable"; }) {
+    overlays = [
+      (import ./overlay.nix)
+    ];
+  }
+}:
+
 let
-  pkgs = import (fetchTarball { url = "channel:nixpkgs-unstable"; }) {};
-  poetry2nix = import ./. { inherit pkgs; inherit poetry; };
-  poetry = pkgs.callPackage ./pkgs/poetry { python = pkgs.python3; inherit poetry2nix; };
+
 in
 pkgs.mkShell {
   buildInputs = [
-    poetry
     pkgs.nixpkgs-fmt
+    pkgs.poetry
   ];
 }
