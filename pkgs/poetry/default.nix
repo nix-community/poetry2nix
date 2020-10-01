1bebfa70f8e0f3914a20874c0f6017caf3c48d50
adisbladis <adisbladis@gmail.com>
2020-10-01 14:36:24 +0200

    poetry: 1.0.10 -> 1.1.0

--- pkgs/poetry/default.nix
+++ pkgs/poetry/default.nix
@@ -14,6 +14,7 @@ poetry2nix.mkPoetryApplication {
 
   # "Vendor" dependencies (for build-system support)
   postPatch = ''
+    echo "import sys" >> poetry/__init__.py
     for path in ''${PYTHONPATH//:/ }; do echo $path; done | uniq | while read path; do
       echo "sys.path.insert(0, \"$path\")" >> poetry/__init__.py
     done
