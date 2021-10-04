5f4d6b94787627085565d2039e85cff49dea3916
Alex Chalk <contact@alexchalk.net>
2021-10-04 10:29:13 -0400

    chore: Match surrounding formatting

--- overrides.nix
+++ overrides.nix
@@ -2081,11 +2081,9 @@ self: super:
     nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
   });
 
-  uwsgi = super.uwsgi.overridePythonAttrs (
-    old: rec {
-      buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.ncurses ];
-    }
-  );
+  uwsgi = super.uwsgi.overridePythonAttrs (old: {
+    buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.ncurses ];
+  });
 
   virtualenv = super.virtualenv.overridePythonAttrs (old: {
     postPatch = ''
