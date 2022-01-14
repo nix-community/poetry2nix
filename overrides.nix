601609e83be35bbab56af633836edb9d624dcfbf
lunik1 <ch.gpg@themaw.xyz>
2022-01-14 16:51:34 +0000

    overrides.pymssql: add override

--- overrides.nix
+++ overrides.nix
@@ -1335,6 +1335,13 @@ self: super:
     }
   );
 
+  pymssql = super.pymssql.overridePythonAttrs (old: {
+    buildInputs = (old.buildInputs or [ ])
+      ++ [ self.cython pkgs.openssl ];
+    propagatedBuildInputs = (old.propagatedBuildInputs or [ ])
+      ++ [ pkgs.freetds ];
+  });
+
   pyopenssl = super.pyopenssl.overridePythonAttrs (
     old: {
       buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.openssl ];
