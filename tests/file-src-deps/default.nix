ff4c1b906878e0e433a3ef123c5600fc0d340716
adisbladis <adisbladis@gmail.com>
2022-01-18 20:07:35 +1200

    Fix file deps test

--- tests/file-src-deps/default.nix
+++ tests/file-src-deps/default.nix
@@ -6,4 +6,9 @@ poetry2nix.mkPoetryApplication {
   poetrylock = ./poetry.lock;
   src = lib.cleanSource ./.;
   pwd = ./.;
+  overrides = poetry2nix.overrides.withDefaults (self: super: {
+    trivial = super.trivial.overridePythonAttrs (old: {
+      buildInputs = old.buildInputs or [ ] ++ [ self.poetry ];
+    });
+  });
 }
