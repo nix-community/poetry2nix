0a6d22407cb8cecad7e68eb1557818fdceddb5a7
adisbladis <adisbladis@gmail.com>
2022-01-22 01:42:48 +1200

    overrides.matplotlib: Hopefully fix darwin build

--- overrides/default.nix
+++ overrides/default.nix
@@ -803,6 +803,19 @@ lib.composeManyExtensions [
 
           interactive = enableTk || enableGtk3 || enableQt;
 
+          passthru = {
+            config = {
+              directories = { basedirlist = "."; };
+              libs = {
+                system_freetype = true;
+                system_qhull = true;
+              } // lib.optionalAttrs stdenv.isDarwin {
+                # LTO not working in darwin stdenv, see Nixpkgs #19312
+                enable_lto = false;
+              };
+            };
+          };
+
           inherit (pkgs) tk tcl wayland qhull;
           inherit (pkgs.xorg) libX11;
           inherit (pkgs.darwin.apple_sdk.frameworks) Cocoa;
@@ -810,10 +823,24 @@ lib.composeManyExtensions [
         {
           XDG_RUNTIME_DIR = "/tmp";
 
-          buildInputs = (old.buildInputs or [ ])
-            ++ lib.optional enableGhostscript pkgs.ghostscript
-            ++ lib.optional stdenv.isDarwin [ Cocoa ]
-            ++ [ self.certifi ];
+          buildInputs = old.buildInputs or [ ] ++ [
+            pkgs.which
+          ] ++ lib.optional enableGhostscript [
+            pkgs.ghostscript
+          ] ++ lib.optional stdenv.isDarwin [
+            Cocoa
+          ];
+
+          propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
+            self.certifi
+            pkgs.libpng
+            pkgs.freetype
+            qhull
+          ]
+            ++ lib.optionals enableGtk3 [ pkgs.cairo self.pycairo pkgs.gtk3 pkgs.gobject-introspection self.pygobject3 ]
+            ++ lib.optionals enableTk [ pkgs.tcl pkgs.tk self.tkinter pkgs.libX11 ]
+            ++ lib.optionals enableQt [ self.pyqt5 ]
+          ;
 
           nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [
             pkg-config
@@ -822,25 +849,9 @@ lib.composeManyExtensions [
             self.setuptools-scm-git-archive
           ];
 
-          passthru.config = {
-            directories = { basedirlist = "."; };
-            libs = {
-              system_freetype = true;
-              system_qhull = true;
-            } // lib.optionalAttrs stdenv.isDarwin {
-              # LTO not working in darwin stdenv, see Nixpkgs #19312
-              enable_lto = false;
-            };
-          };
+          passthru = old.passthru or { } // passthru;
 
-          MPLSETUPCFG = pkgs.writeText "mplsetup.cfg" ''
-            [libs]
-            system_freetype = True
-            system_qhull = True
-          '' + lib.optionalString stdenv.isDarwin ''
-            # LTO not working in darwin stdenv, see NixOS/nixpkgs/pull/19312
-            enable_lto = false
-          '';
+          MPLSETUPCFG = pkgs.writeText "mplsetup.cfg" (lib.generators.toINI { } passthru.config);
 
           # Matplotlib tries to find Tcl/Tk by opening a Tk window and asking the
           # corresponding interpreter object for its library paths. This fails if
@@ -859,23 +870,14 @@ lib.composeManyExtensions [
               substituteInPlace src/_c_internal_utils.c \
                 --replace libX11.so.6 ${libX11}/lib/libX11.so.6 \
                 --replace libwayland-client.so.0 ${wayland}/lib/libwayland-client.so.0
+            '' +
+            # avoid matplotlib trying to download dependencies
+            ''
+              echo "[libs]
+              system_freetype=true
+              system_qhull=true" > mplsetup.cfg
             '';
 
-          propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
-            pkgs.libpng
-            pkgs.freetype
-            qhull
-          ]
-            ++ lib.optionals enableGtk3 [ pkgs.cairo self.pycairo pkgs.gtk3 pkgs.gobject-introspection self.pygobject3 ]
-            ++ lib.optionals enableTk [ pkgs.tcl pkgs.tk self.tkinter pkgs.libX11 ]
-            ++ lib.optionals enableQt [ self.pyqt5 ]
-          ;
-
-          preBuild = ''
-            cp -r ${pkgs.qhull} .
-          '';
-
-          inherit (super.matplotlib) patches;
         }
       );
 
