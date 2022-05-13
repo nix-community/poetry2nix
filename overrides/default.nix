5c6b2ab17bc7df9291ee258cc3c562bcfcb32e60
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-05-13 08:30:51 -0400

    shapely: use python3 instead of minimal

--- overrides/default.nix
+++ overrides/default.nix
@@ -1891,7 +1891,7 @@ lib.composeManyExtensions [
 
           # Fix library paths
           postPatch = old.postPatch or "" + ''
-            ${pkgs.python3Minimal.interpreter} ${./shapely-rewrite.py} shapely/geos.py
+            ${pkgs.python3.interpreter} ${./shapely-rewrite.py} shapely/geos.py
           '';
         }
       );
