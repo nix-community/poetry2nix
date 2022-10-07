81fb0b72335e989290d4e018987e432153581560
adisbladis <adisbladis@gmail.com>
2022-10-07 23:09:42 +1300

    overrides: add more build systems

--- tests/ansible-molecule/default.nix
+++ tests/ansible-molecule/default.nix
@@ -1,8 +1,12 @@
 { lib, poetry2nix, python3 }:
 
-poetry2nix.mkPoetryApplication {
+(poetry2nix.mkPoetryApplication {
   python = python3;
   pyproject = ./pyproject.toml;
   poetrylock = ./poetry.lock;
   src = lib.cleanSource ./.;
-}
+}).overridePythonAttrs (old: {
+  nativeBuildInputs = old.nativeBuildInputs or [ ] ++ [
+    old.passthru.python.pkgs.setuptools
+  ];
+})
