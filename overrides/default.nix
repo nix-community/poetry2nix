b76b4a7067b7b2e550908c9fcd13035b60ab1a7b
Jairo Llopis <yajo.sk8@gmail.com>
2022-10-07 09:11:23 +0100

    overrides: add copier
    Without git, Copier loses lots of its functionality.

--- overrides/default.nix
+++ overrides/default.nix
@@ -341,6 +341,10 @@ lib.composeManyExtensions [
         }
       );
 
+      copier = super.copier.overrideAttrs (old: {
+        propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [ pkgs.git ];
+      });
+
       cryptography =
         let
           getCargoHash = version: {
