07e74f9b53482f008cdd5955ecd5c2d478a518a7
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2022-12-17 18:56:59 -0500

    test: add failing grpcio wheel test

--- tests/grpcio-wheel/default.nix
+++ tests/grpcio-wheel/default.nix
@@ -4,9 +4,20 @@ let
     python = python3;
     pyproject = ./pyproject.toml;
     poetrylock = ./poetry.lock;
-    preferWheels = true;
+    overrides = poetry2nix.overrides.withDefaults
+      # This is also in overrides.nix but repeated for completeness
+      (
+        self: super: {
+          grpcio = super.grpcio.override {
+            preferWheel = true;
+          };
+        }
+      );
   };
+
+  isWheelGrpcIO = env.python.pkgs.grpcio.src.isWheel or false;
+
 in
-runCommand "grpcio-wheel" { } ''
+assert isWheelGrpcIO; runCommand "grpcio-wheel" { } ''
   ${env}/bin/python -c 'import grpc; print(grpc.__version__)' > $out
 ''
