9006db54aaa812f2c02c3777aef47f03d6d47de3
Sergei Trofimovich <slyich@gmail.com>
2022-12-17 21:06:39 +0000

    overrides: drop unused pkgs.xlibsWrapper package
    `xlibsWrapper` was a transitional package to move from monolithic
    xorg package to modules. `nixpkgs` `python3Packages.pycairo` already
    removed `pkgs.xlibsWrapper`. This change follow the same change.
    
    Bug: https://github.com/NixOS/nixpkgs/issues/194054
    Closes: https://github.com/nix-community/poetry2nix/issues/863

--- overrides/default.nix
+++ overrides/default.nix
@@ -1682,7 +1682,6 @@ lib.composeManyExtensions [
 
             propagatedBuildInputs = (old.propagatedBuildInputs or [ ]) ++ [
               pkgs.cairo
-              pkgs.xlibsWrapper
             ];
 
             mesonFlags = [ "-Dpython=${if self.isPy3k then "python3" else "python"}" ];
