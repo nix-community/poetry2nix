4ee1b00b21f7b6aa0bd974f9425df3563bf2a6e6
Victor Engmark <vengmark@linz.govt.nz>
2023-01-30 14:36:15 +1300

    refactor: Simplify Shapely test

--- tests/shapely/default.nix
+++ tests/shapely/default.nix
@@ -1,8 +1,4 @@
-{ lib, poetry2nix, python38 }:
-
+{ poetry2nix }:
 poetry2nix.mkPoetryApplication {
-  python = python38;
-  pyproject = ./pyproject.toml;
-  poetrylock = ./poetry.lock;
-  src = lib.cleanSource ./.;
+  projectDir = ./.;
 }
