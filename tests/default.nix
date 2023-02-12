7f8501f43a661e0f37dafe4a34612fcb1d72b8d8
Victor Engmark <vengmark@linz.govt.nz>
2023-02-13 10:52:07 +1300

    refactor: Simplify virtualenv dependencies
    At least Virtualenv 16.7.12 and 20.17.1 build with just the
    "setuptools-scm" build system, so we can probably drop "cython" and
    "setuptools".

--- tests/default.nix
+++ tests/default.nix
@@ -151,5 +151,6 @@ builtins.removeAttrs
   wheel-wheel = callTest ./wheel-wheel { };
   fancycompleter-wheel = callTest ./fancycompleter-wheel { };
   rfc3986-validator = callTest ./rfc3986-validator { };
+  virtualenv-pre-20-18 = callTest ./virtualenv-pre-20-18 { };
 }
   skipTests
