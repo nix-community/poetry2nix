573b643d7e4bf3f03047cc0829ef791b21431843
adisbladis <adisbladis@gmail.com>
2023-03-23 12:04:35 +1300

    tests.pendulum: Use python 3.8
    MacOS ci is unreliable and fails with the wheel install on python 3.10.
    My guess is that it's an older version of setuptools that's causing this.

--- tests/pendulum/default.nix
+++ tests/pendulum/default.nix
@@ -1,6 +1,7 @@
-{ lib, poetry2nix, runCommand }:
+{ lib, poetry2nix, runCommand, python38 }:
 poetry2nix.mkPoetryApplication {
   pyproject = ./pyproject.toml;
   poetrylock = ./poetry.lock;
   src = lib.cleanSource ./.;
+  python = python38;
 }
