ae50db0e94f2a45e118913751cb1de72e769465b
Bruno Claudino <bruarrais@gmail.com>
2023-03-27 23:48:43 -0300

    orjson fake sha

--- overrides/default.nix
+++ overrides/default.nix
@@ -1517,6 +1517,7 @@ lib.composeManyExtensions [
             "3.8.5" = "sha256-JtUCJ3TP9EKGcddeyW1e/72k21uKneq9SnZJeLvn9Os=";
             "3.8.6" = "sha256-8T//q6nQoZhh8oJWDCeQf3gYRew58dXAaxkYELY4CJM=";
             "3.8.7" = "sha256-JBO8nl0sC+XIn17vI7hC8+nA1HYI9jfvZrl9nCE3k1s=";
+            "3.8.8" = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
           }.${version} or (
             lib.warn "Unknown orjson version: '${version}'. Please update getCargoHash." lib.fakeHash
           );
