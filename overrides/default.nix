6c5c7745fd30b7dea387f657f20f94e2481c3ca6
Cobalt <cobalt@cobalt.rocks>
2023-04-29 19:44:12 +0200

    fix: adjust overwrite to current duckdb package structure

--- overrides/default.nix
+++ overrides/default.nix
@@ -581,6 +581,8 @@ lib.composeManyExtensions [
 
       duckdb = super.duckdb.overridePythonAttrs (old: {
         postPatch = lib.optionalString (!(old.src.isWheel or false)) ''
+          cd tools/pythonpkg
+
           substituteInPlace setup.py \
             --replace 'multiprocessing.cpu_count()' "$NIX_BUILD_CORES" \
             --replace 'setuptools_scm<7.0.0' 'setuptools_scm'
