30e7c136ff08227b5ecbefc929e6f622dda53888
Jean-Baptiste Giraudeau <jb@giraudeau.info>
2023-05-03 15:36:31 +0200

    Fix failing pytest-metadata test with documented workaround

--- tests/pytest-metadata/default.nix
+++ tests/pytest-metadata/default.nix
@@ -5,4 +5,22 @@ poetry2nix.mkPoetryApplication {
   pyproject = ./pyproject.toml;
   poetrylock = ./poetry.lock;
   src = lib.cleanSource ./.;
+  # Workaround `ModuleNotFoundError: No module named 'poetry'`
+  # cf. https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+  overrides =
+    let
+      pypkgs-build-requirements = {
+        pytest-metadata = [ "poetry" ];
+        pytest-select = [ "poetry" ];
+      };
+    in
+    poetry2nix.defaultPoetryOverrides.extend (self: super:
+      builtins.mapAttrs
+        (package: build-requirements:
+          (builtins.getAttr package super).overridePythonAttrs (old: {
+            buildInputs = (old.buildInputs or [ ]) ++ (builtins.map (pkg: if builtins.isString pkg then builtins.getAttr pkg super else pkg) build-requirements);
+          })
+        )
+        pypkgs-build-requirements
+    );
 }
