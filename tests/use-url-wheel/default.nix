962f1a59921a4acc5e949f937fa1ce74df1a1fc3
pegasust <pegasucksgg@gmail.com>
2023-08-30 20:14:39 -0700

    ref: format test

--- tests/use-url-wheel/default.nix
+++ tests/use-url-wheel/default.nix
@@ -19,8 +19,9 @@ let
       { } ''
       "${app}/bin/test" >$out
     '';
-  is_wheel_attr_test = x: lib.warnIf (!url_is_wheel) 
-    "url should resolve to have src with .isWheel, likely darwin only issue" x;
+  is_wheel_attr_test = x: lib.warnIf (!url_is_wheel)
+    "url should resolve to have src with .isWheel, likely darwin only issue"
+    x;
   is_wheel_test = x: assert lib.strings.hasSuffix "whl" url_nix_store; x;
   # HACK: CI fails because https://github.com/nix-community/poetry2nix/pull/1109
   # seems to want libcuda to be installed/managed separately, run it on impure
@@ -28,8 +29,8 @@ let
   integration_run_test = x: assert (builtins.readFile output) == "Dies ist ein Testsatz.\n"; x;
   app_builds = x: assert lib.isDerivation app; x;
 
-  constraintOutput = x: lib.pipe x [is_wheel_attr_test is_wheel_test app_builds];
+  constraintOutput = x: lib.pipe x [ is_wheel_attr_test is_wheel_test app_builds ];
 
 in
-  constraintOutput
+constraintOutput
   app
