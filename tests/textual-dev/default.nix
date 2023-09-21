499f61b162ddabdbd2ff7f558dda017a1ed45f01
Steve Purcell <steve@sanityinc.com>
2023-09-21 16:31:16 +0200

    Additional build-systems entries

---
+++ tests/textual-dev/default.nix
@@ -0,0 +1,11 @@
+{ lib, poetry2nix, python3, runCommand }:
+let
+  env = poetry2nix.mkPoetryEnv {
+    python = python3;
+    pyproject = ./pyproject.toml;
+    poetrylock = ./poetry.lock;
+  };
+in
+runCommand "textual-dev-test" { } ''
+  ${env}/bin/python -c 'import textual_dev' > $out
+''
