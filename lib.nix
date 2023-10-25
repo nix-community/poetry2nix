960343e97cb82b0ba7e7211451f7740968840801
adisbladis <adisbladis@gmail.com>
2023-10-25 22:16:15 +1300

    Use PEP-599 platform definitions from pyproject.nix

--- lib.nix
+++ lib.nix
@@ -203,23 +203,6 @@ let
       };
     };
 
-  # Maps Nixpkgs CPU values to target machines known to be supported for manylinux* wheels.
-  # (a.k.a. `uname -m` output from CentOS 7)
-  #
-  # This is current as of manylinux2014 (PEP-0599), and is a superset of manylinux2010 / manylinux1.
-  # s390x is not supported in Nixpkgs, so we don't map it.
-  manyLinuxTargetMachines = {
-    x86_64 = "x86_64";
-    i686 = "i686";
-    aarch64 = "aarch64";
-    armv7l = "armv7l";
-    powerpc64 = "ppc64";
-    powerpc64le = "ppc64le";
-  };
-
-  # Machine tag for our target platform (if available)
-  getTargetMachine = stdenv: manyLinuxTargetMachines.${stdenv.targetPlatform.parsed.cpu.name} or null;
-
 in
 {
   inherit
@@ -232,6 +215,5 @@ in
     satisfiesSemver
     cleanPythonSources
     getPythonVersion
-    getTargetMachine
     ;
 }
