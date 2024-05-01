4807e16e0d907112a0231b70f4b3494ec3cf1d70
vetsin <131726+vetsin@users.noreply.github.com>
2024-05-01 11:13:16 -0700

    fix: evaluate the new jake test

--- tests/jake/default.nix
+++ tests/jake/default.nix
@@ -1,5 +1,5 @@
 { lib, poetry2nix, python3, runCommand }:
-{
+let
   envWheel = poetry2nix.mkPoetryEnv {
     python = python3;
     projectDir = ./.;
@@ -9,4 +9,9 @@
     python = python3;
     projectDir = ./.;
   };
-}
+in
+runCommand "jake-test" { } ''
+  ${envWheel}/bin/python -c 'import jake'
+  ${env}/bin/python -c 'import jake'
+  touch $out
+''
