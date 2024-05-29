92ae9164739e2f5c231d3ad9cf8a8616187a068b
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-05-29 07:13:26 -0400

    feat(ckzg): build and test

---
+++ tests/ckzg/default.nix
@@ -0,0 +1,12 @@
+{ poetry2nix, python3, runCommand }:
+let
+  env = poetry2nix.mkPoetryEnv {
+    python = python3;
+    pyproject = ./pyproject.toml;
+    poetrylock = ./poetry.lock;
+  };
+in
+runCommand "ckzg-test" { } ''
+  ${env}/bin/python -c 'import ckzg'
+  touch $out
+''
