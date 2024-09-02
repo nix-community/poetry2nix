184203e05a78f93f3c6329597db4cbec0471984b
adisbladis <adisbladis@gmail.com>
2024-09-03 01:25:44 +1200

    vendor: Upgrade pyproject.nix
    This includes a full rewrite of the PEP-508 parser which performs ~2x as fast with ~0.4x the RAM.

--- vendor/pyproject.nix/lib/pep656.nix
+++ vendor/pyproject.nix/lib/pep656.nix
@@ -1,17 +1,23 @@
 { pep599, ... }:
 let
-  inherit (builtins) match elemAt compareVersions splitVersion;
+  inherit (builtins)
+    match
+    elemAt
+    compareVersions
+    splitVersion
+    ;
 
 in
 
 {
-  /* Check if a musllinux tag is compatible with a given stdenv.
+  /*
+    Check if a musllinux tag is compatible with a given stdenv.
 
-     Type: muslLinuxTagCompatible :: AttrSet -> derivation -> string -> bool
+    Type: muslLinuxTagCompatible :: AttrSet -> derivation -> string -> bool
 
-     Example:
-     # muslLinuxTagCompatible pkgs.stdenv.targetPlatform pkgs.stdenv.cc.libc "musllinux_1_1_x86_64"
-     true
+    Example:
+    # muslLinuxTagCompatible pkgs.stdenv.targetPlatform pkgs.stdenv.cc.libc "musllinux_1_1_x86_64"
+    true
   */
   muslLinuxTagCompatible =
     # Platform attrset (`lib.systems.elaborate "x86_64-linux"`)
@@ -22,19 +28,24 @@ in
     tag:
     let
       m = match "musllinux_([0-9]+)_([0-9]+)_(.*)" tag;
-      mAt = elemAt m;
-      tagMajor = mAt 0;
-      tagMinor = mAt 1;
-      tagArch = mAt 2;
+      tagMajor = elemAt m 0;
+      tagMinor = elemAt m 1;
+      tagArch = elemAt m 2;
       sysVersion' = elemAt (splitVersion libc.version);
       sysMajor = sysVersion' 0;
       sysMinor = sysVersion' 1;
     in
-    if m == null then throw "'${tag}' is not a valid musllinux tag."
-    else if platform.libc != "musl" then false
-    else if libc.pname != "musl" then false
-    else if compareVersions "${sysMajor}.${sysMinor}" "${tagMajor}.${tagMinor}" < 0 then false
-    else if pep599.manyLinuxTargetMachines.${tagArch} != platform.parsed.cpu.name then false
-    else true;
+    if m == null then
+      throw "'${tag}' is not a valid musllinux tag."
+    else if platform.libc != "musl" then
+      false
+    else if libc.pname != "musl" then
+      false
+    else if compareVersions "${sysMajor}.${sysMinor}" "${tagMajor}.${tagMinor}" < 0 then
+      false
+    else if pep599.manyLinuxTargetMachines.${tagArch} != platform.parsed.cpu.name then
+      false
+    else
+      true;
 
 }
--- vendor/pyproject.nix/lib/project.nix
+++ vendor/pyproject.nix/lib/project.nix
@@ -1,4 +1,12 @@
-{ pep621, poetry, pip, lib, renderers, validators, ... }:
+{
+  pep621,
+  poetry,
+  pip,
+  lib,
+  renderers,
+  validators,
+  ...
+}:
 
 let
   inherit (builtins) mapAttrs;
@@ -6,95 +14,138 @@ let
   # Map over renderers and inject project argument.
   # This allows for a user interface like:
   # project.renderers.buildPythonPackage { } where project is already curried.
-  curryProject = attrs: project: lib.mapAttrs (_: func: args: func (args // { inherit project; })) attrs;
+  curryProject =
+    attrs: project:
+    lib.mapAttrs (
+      _: func: args:
+      func (args // { inherit project; })
+    ) attrs;
+
+  # Package manager specific extensions.
+  # Remap extension fields to optional-dependencies
+  uvListPaths = {
+    "tool.uv.dev-dependencies" = "dev-dependencies";
+  };
+  pdmAttrPaths = [ "tool.pdm.dev-dependencies" ];
 
 in
 lib.fix (self: {
-  /* Load dependencies from a PEP-621 pyproject.toml.
+  /*
+    Load dependencies from a PEP-621 pyproject.toml.
 
-     Type: loadPyproject :: AttrSet -> AttrSet
+    Type: loadPyproject :: AttrSet -> AttrSet
 
-     Example:
-       # loadPyproject { pyproject = lib.importTOML }
-       {
-         dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
-         build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
-         pyproject = { }; # The unmarshaled contents of pyproject.toml
-         projectRoot = null; # Path to project root
-         requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
-       }
+    Example:
+      # loadPyproject { pyproject = lib.importTOML }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = { }; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
   */
   loadPyproject =
     {
       # The unmarshaled contents of pyproject.toml
-      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml")
-    , # Example: extrasAttrPaths = [ "tool.pdm.dev-dependencies" ];
-      extrasAttrPaths ? [ ]
-    , # Path to project root
-      projectRoot ? null
-    ,
-    }: lib.fix (project: {
-      dependencies = pep621.parseDependencies { inherit pyproject extrasAttrPaths; };
+      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml"),
+      # Example: extrasAttrPaths = [ "tool.pdm.dev-dependencies" ];
+      extrasAttrPaths ? [ ],
+      # Example: extrasListPaths = { "tool.uv.dependencies.dev-dependencies" = "dev-dependencies"; }
+      extrasListPaths ? { },
+      # Path to project root
+      projectRoot ? null,
+    }:
+    lib.fix (project: {
+      dependencies = pep621.parseDependencies { inherit pyproject extrasAttrPaths extrasListPaths; };
       inherit pyproject projectRoot;
       renderers = curryProject renderers project;
       validators = curryProject validators project;
       requires-python = pep621.parseRequiresPython pyproject;
     });
 
-  /* Load dependencies from a PDM pyproject.toml.
+  /*
+    Load dependencies from a uv pyproject.toml.
 
-     Type: loadPDMPyproject :: AttrSet -> AttrSet
+    Type: loadUVPyproject :: AttrSet -> AttrSet
 
-     Example:
-       # loadPyproject { pyproject = lib.importTOML }
-       {
-         dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
-         build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
-         pyproject = { }; # The unmarshaled contents of pyproject.toml
-         projectRoot = null; # Path to project root
-         requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
-       }
+    Example:
+      # loadUVPyproject { projectRoot = ./.; }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = { }; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
+  */
+  loadUVPyproject =
+    {
+      # The unmarshaled contents of pyproject.toml
+      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml"),
+      # Path to project root
+      projectRoot ? null,
+    }:
+    self.loadPyproject {
+      inherit pyproject projectRoot;
+      extrasListPaths = uvListPaths;
+    };
+
+  /*
+    Load dependencies from a PDM pyproject.toml.
+
+    Type: loadPDMPyproject :: AttrSet -> AttrSet
+
+    Example:
+      # loadPyproject { projectRoot = ./.; }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = { }; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
   */
   loadPDMPyproject =
     {
       # The unmarshaled contents of pyproject.toml
-      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml")
-    , # Path to project root
-      projectRoot ? null
-    , # The unmarshaled contents of pdm.lock
-      pdmLock ? lib.importTOML (projectRoot + "/pdm.lock")
-    ,
-    }: self.loadPyproject
-      {
-        inherit pyproject projectRoot;
-        extrasAttrPaths = [ "tool.pdm.dev-dependencies" ];
-      } // {
+      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml"),
+      # Path to project root
+      projectRoot ? null,
+      # The unmarshaled contents of pdm.lock
+      pdmLock ? lib.importTOML (projectRoot + "/pdm.lock"),
+    }:
+    self.loadPyproject {
+      inherit pyproject projectRoot;
+      extrasAttrPaths = pdmAttrPaths;
+    }
+    // {
       inherit pdmLock;
     };
 
-  /* Load dependencies from a Poetry pyproject.toml.
+  /*
+    Load dependencies from a Poetry pyproject.toml.
 
-     Type: loadPoetryPyproject :: AttrSet -> AttrSet
+    Type: loadPoetryPyproject :: AttrSet -> AttrSet
 
-     Example:
-       # loadPoetryPyproject { pyproject = lib.importTOML }
-       {
-         dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
-         build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
-         pyproject = { }; # The unmarshaled contents of pyproject.toml
-         projectRoot = null; # Path to project root
-         requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
-       }
+    Example:
+      # loadPoetryPyproject { projectRoot = ./.; }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = { }; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
   */
   loadPoetryPyproject =
     {
       # The unmarshaled contents of pyproject.toml
-      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml")
-    , # Path to project root
-      projectRoot ? null
-    , # The unmarshaled contents of pyproject.toml
-      poetryLock ? lib.importTOML (projectRoot + "/poetry.lock")
-    ,
+      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml"),
+      # Path to project root
+      projectRoot ? null,
+      # The unmarshaled contents of poetry.lock
+      poetryLock ? lib.importTOML (projectRoot + "/poetry.lock"),
     }:
     let
       pyproject-pep621 = poetry.translatePoetryProject pyproject;
@@ -109,30 +160,31 @@ lib.fix (self: {
       requires-python = null;
     });
 
-  /* Load dependencies from a requirements.txt.
+  /*
+    Load dependencies from a requirements.txt.
 
-     Note that as requirements.txt is lacking important project metadata this is incompatible with some renderers.
+    Note that as requirements.txt is lacking important project metadata this is incompatible with some renderers.
 
-     Type: loadRequirementsTxt :: AttrSet -> AttrSet
+    Type: loadRequirementsTxt :: AttrSet -> AttrSet
 
-     Example:
-       # loadRequirementstxt { requirements = builtins.readFile ./requirements.txt; root = ./.; }
-       {
-         dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
-         build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
-         pyproject = null; # The unmarshaled contents of pyproject.toml
-         projectRoot = null; # Path to project root
-         requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
-       }
+    Example:
+      # loadRequirementstxt { requirements = builtins.readFile ./requirements.txt; projectRoot = ./.; }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = null; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
   */
   loadRequirementsTxt =
     {
       # The contents of requirements.txt
-      requirements ? builtins.readFile (projectRoot + "/requirements.txt")
-    , # Path to project root
-      projectRoot ? null
-    ,
-    }: lib.fix (project: {
+      requirements ? builtins.readFile (projectRoot + "/requirements.txt"),
+      # Path to project root
+      projectRoot ? null,
+    }:
+    lib.fix (project: {
       dependencies = {
         dependencies = map (x: x.requirement) (pip.parseRequirementsTxt requirements);
         extras = { };
@@ -145,34 +197,42 @@ lib.fix (self: {
       requires-python = null;
     });
 
-  /* Load dependencies from a either a PEP-621 or Poetry pyproject.toml file.
-     This function is intended for 2nix authors that wants to include local pyproject.toml files
-     but don't know up front whether they're from Poetry or PEP-621.
+  /*
+    Load dependencies from a either a PEP-621 or Poetry pyproject.toml file.
+    This function is intended for 2nix authors that wants to include local pyproject.toml files
+    but don't know up front whether they're from Poetry or PEP-621.
 
-     Type: loadPyprojectDynamic :: AttrSet -> AttrSet
+    Type: loadPyprojectDynamic :: AttrSet -> AttrSet
 
-     Example:
-       # loadPyprojectDynamic { pyproject = lib.importTOML }
-       {
-         dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
-         build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
-         pyproject = { }; # The unmarshaled contents of pyproject.toml
-         projectRoot = null; # Path to project root
-         requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
-       }
+    Example:
+      # loadPyprojectDynamic { projectRoot = ./.; }
+      {
+        dependencies = { }; # Parsed dependency structure in the schema of `lib.pep621.parseDependencies`
+        build-systems = [ ];  # Returned by `lib.pep518.parseBuildSystems`
+        pyproject = { }; # The unmarshaled contents of pyproject.toml
+        projectRoot = null; # Path to project root
+        requires-python = null; # requires-python as parsed by pep621.parseRequiresPython
+      }
   */
   loadPyprojectDynamic =
     {
       # The unmarshaled contents of pyproject.toml
-      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml")
-    , # Path to project root
-      projectRoot ? null
+      pyproject ? lib.importTOML (projectRoot + "/pyproject.toml"),
+      # Path to project root
+      projectRoot ? null,
     }:
     let
-      isPoetry = lib.hasAttrByPath [ "tool" "poetry" ] pyproject;
-      isPep621 = lib.hasAttrByPath [ "project" ] pyproject;
+      isPoetry = pyproject ? tool.poetry;
+      isPep621 = pyproject ? project;
     in
-    (if isPoetry then self.loadPoetryPyproject else if isPep621 then self.loadPyproject else throw "Project is neither Poetry nor PEP-621") {
-      inherit pyproject projectRoot;
-    };
+    if isPoetry then
+      self.loadPoetryPyproject { inherit pyproject projectRoot; }
+    else if isPep621 then
+      self.loadPyproject {
+        inherit pyproject projectRoot;
+        extrasListPaths = uvListPaths;
+        extrasAttrPaths = pdmAttrPaths;
+      }
+    else
+      throw "Project is neither Poetry nor PEP-621";
 })
