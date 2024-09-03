46c0b74f2014b5d39c82d95d3555059284500760
chn <chn@chn.moe>
2024-09-03 17:36:01 +0800

    fix(overrides): add build system deps for nglview

--- overrides/default.nix
+++ overrides/default.nix
@@ -1780,6 +1780,15 @@ lib.composeManyExtensions [
         }
       );
 
+      nglview = prev.nglview.overridePythonAttrs (
+        old: {
+          nativeBuildInputs = old.nativeBuildInputs or [ ] ++ (with final; [
+            jupyter-packaging
+            versioneer
+          ]);
+        }
+      );
+
       numpy = prev.numpy.overridePythonAttrs (
         old:
         let
