677d5456e24f958fdeb8de5638b4e387853550b5
yihuang <huang@crypto.com>
2024-09-27 16:13:04 +0800

    Update templates/app/flake.nix

--- templates/app/flake.nix
+++ templates/app/flake.nix
@@ -24,7 +24,8 @@
                   nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: final.${a}) systems;
                 }))
               {
-                x25519 = [ "setuptools" ];
+                # https://github.com/nix-community/poetry2nix/blob/master/docs/edgecases.md#modulenotfounderror-no-module-named-packagename
+                # package = [ "setuptools" ];
               }
           );
         };
