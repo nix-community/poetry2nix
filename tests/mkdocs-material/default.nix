7b0e52ce0c22ccbc6dfbcc3c442e27d08c8bee72
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-28 13:30:50 -0400

    test: try using a wheel for watchdog

--- tests/mkdocs-material/default.nix
+++ tests/mkdocs-material/default.nix
@@ -1,9 +1,12 @@
-{ poetry2nix, python311 }:
+{ poetry2nix, python3 }:
 let
   env = poetry2nix.mkPoetryEnv {
-    python = python311;
+    python = python3;
     pyproject = ./pyproject.toml;
     poetrylock = ./poetry.lock;
+    overrides = poetry2nix.overrides.withDefaults (_: prev: {
+      watchdog = prev.watchdog.override { preferWheel = true; };
+    });
   };
 in
 env.python.pkgs.mkdocs-material
