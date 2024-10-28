1153461005cbf3d4a85feee4a5f10626318ea725
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2024-10-28 13:38:22 -0400

    test: only use wheel on x86_64-darwin

--- tests/mkdocs-material/default.nix
+++ tests/mkdocs-material/default.nix
@@ -1,11 +1,13 @@
-{ poetry2nix, python3 }:
+{ poetry2nix, python3, stdenv }:
 let
   env = poetry2nix.mkPoetryEnv {
     python = python3;
     pyproject = ./pyproject.toml;
     poetrylock = ./poetry.lock;
     overrides = poetry2nix.overrides.withDefaults (_: prev: {
-      watchdog = prev.watchdog.override { preferWheel = true; };
+      watchdog = prev.watchdog.override {
+        preferWheel = stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64;
+      };
     });
   };
 in
