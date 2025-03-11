dd481e5f9f4f06c7caa43269ccf472b548e2af75
James Brock <jamesbrock@gmail.com>
2025-03-11 09:38:32 +0900

    test: 2 new tests for mkPoetryPackages .whl dependencies
    Test succeeds because poetry2nix can select dependency markers for remote .whl
    files:
    
    nix-build --attr mk-poetry-packages-partition-whl-remote --keep-going --show-trace tests/default.nix
    
    Test fails because poetry2nix cannot select dependency markers for local .whl
    files:
    
    nix-build --attr mk-poetry-packages-partition-whl-local --keep-going --show-trace tests/default.nix

---
+++ tests/mk-poetry-packages-partition-whl-local/default.nix
@@ -0,0 +1,20 @@
+{ stdenv, poetry2nix, python312, runCommand }:
+if (stdenv.isDarwin && stdenv.isx86_64) then
+# Disable this test for the case of Darwin x86_64.
+# I can't get poetry2nix CI to pass for that case, not sure why.
+  runCommand "mk-poetry-packages-partition-whl-local-test" { } ''
+    echo "skip test"
+    touch $out
+  ''
+else
+  let
+    env = poetry2nix.mkPoetryEnv {
+      python = python312;
+      projectDir = ./.;
+      pyproject = ./pyproject.toml;
+      poetrylock = ./poetry.lock;
+    };
+  in runCommand "mk-poetry-packages-partition-whl-local-test" { } ''
+    ${env}/bin/python -c 'import watchdog; print(watchdog.__name__)'
+    touch $out
+  ''
