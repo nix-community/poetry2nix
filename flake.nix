e7bf63446492f4b62316bcbbcd82f706f942b883
Eman Resu <78693624+quatquatt@users.noreply.github.com>
2025-06-13 16:01:40 -0400

    flake: split actions into separate file

--- flake.nix
+++ flake.nix
@@ -20,9 +20,9 @@
       self,
       nixpkgs,
       flake-utils,
-      nix-github-actions,
       treefmt-nix,
-    }:
+      ...
+    } @ inputs:
     let
       systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
       eachSystem = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
@@ -32,65 +32,7 @@
       overlays.default = nixpkgs.lib.composeManyExtensions [ (import ./overlay.nix) ];
       lib.mkPoetry2Nix = { pkgs }: import ./default.nix { inherit pkgs; };
 
-      githubActions =
-        let
-          mkPkgs =
-            system:
-            import nixpkgs {
-              config = {
-                allowAliases = false;
-                allowInsecurePredicate = _: true;
-              };
-              overlays = [ self.overlays.default ];
-              inherit system;
-            };
-        in
-        nix-github-actions.lib.mkGithubMatrix {
-          platforms = {
-            "x86_64-linux" = "ubuntu-22.04";
-            "x86_64-darwin" = "macos-13";
-            "aarch64-darwin" = "macos-14";
-          };
-          checks = {
-            x86_64-linux =
-              let
-                pkgs = mkPkgs "x86_64-linux";
-              in
-              import ./tests { inherit pkgs; }
-              // {
-                formatting = treefmtEval.x86_64-linux.config.build.check self;
-              };
-
-            x86_64-darwin =
-              let
-                pkgs = mkPkgs "x86_64-darwin";
-                inherit (pkgs) lib;
-                tests = import ./tests { inherit pkgs; };
-              in
-              {
-                # Aggregate all tests into one derivation so that only one GHA runner is scheduled for all darwin jobs
-                aggregate = pkgs.runCommand "darwin-aggregate" {
-                  env.TEST_INPUTS = lib.concatStringsSep " " (
-                    lib.attrValues (lib.filterAttrs (_: v: lib.isDerivation v) tests)
-                  );
-                } "touch $out";
-              };
-            aarch64-darwin =
-              let
-                pkgs = mkPkgs "aarch64-darwin";
-                inherit (pkgs) lib;
-                tests = import ./tests { inherit pkgs; };
-              in
-              {
-                # Aggregate all tests into one derivation so that only one GHA runner is scheduled for all darwin jobs
-                aggregate = pkgs.runCommand "darwin-aggregate" {
-                  env.TEST_INPUTS = lib.concatStringsSep " " (
-                    lib.attrValues (lib.filterAttrs (_: v: lib.isDerivation v) tests)
-                  );
-                } "touch $out";
-              };
-          };
-        };
+      githubActions = import ./actions.nix { inherit inputs self; };
 
       templates = {
         app = {
